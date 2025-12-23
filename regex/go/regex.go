df2de3a623a6475b854fbe8a12647aa35fc92920
vatsa <vatsan600@gmail.com>
2025-12-23 10:29:43 +0530

    toNfa and tokenToNfa methods

--- regex/go/regex.go
+++ regex/go/regex.go
@@ -207,3 +207,52 @@ func parseRepeatSpecified(regex string, ctx *parseContext) {
 		},
 	}
 }
+
+type state struct {
+	start       bool
+	terminal    bool
+	transitions map[uint8][]*state
+}
+
+func tokenToNfa(t *token) (*state, *state) {
+	start := &state{
+		transitions: map[uint8][]*state{},
+	}
+	end := &state{
+		transitions: map[uint8][]*state{},
+	}
+	switch t.tokenType {
+	case literal:
+	case or:
+	case bracket:
+	case group, groupUncaptured:
+	case repeat:
+	default:
+		panic("unkown type of token")
+	}
+	return start, end
+}
+
+const epsilonChar uint8 = 0
+
+func toNfa(ctx *parseContext) *state {
+	startState, endState := tokenToNfa(&ctx.tokens[0])
+	for i := 0; i < len(ctx.tokens); i++ {
+		startNext, endNext := tokenToNfa(&ctx.tokens[i])
+		endState.transitions[epsilonChar] = append(endState.transitions[epsilonChar], startNext)
+		endState = endNext
+	}
+	start := &state{
+		transitions: map[uint8][]*state{
+			epsilonChar: {startState},
+		},
+		start: true,
+	}
+	end := &state{
+		transitions: map[uint8][]*state{},
+		terminal:    true,
+	}
+	endState.transitions[epsilonChar] = append(endState.transitions[epsilonChar], end)
+
+	return start
+}
